1834119554d7605a61cd6a8b985eec8eb90f9243
John Simeoni <jack.simeoni@gmail.com>
2023-10-12 20:47:44 -0400

    added home.handlebars and made adjusts to view.js

--- controllers/view.js
+++ controllers/view.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { where } = require('sequelize');
+const { User, Request } = require('../models');
 const withAuth = require('../utils/auth');
 
 // router.get('/admin', async (req, res) => {
@@ -19,6 +20,45 @@ const withAuth = require('../utils/auth');
     res.render('newuser')
   });
 
+  // router.get("/dashboard", withAuth, async (req, res) => {
+  //   // Store the bookData in a variable once the promise is resolved.
+  //   const blogPosts = await Posts.findAll({
+  //     include: [{ model: Users }],
+  //     order: [["createdAt", "DESC"]],
+  //     where: {
+  //       user_id: req.session.user_id,
+  //     },
+  //   });
+
+
+  //route to home
+   router.get("/", async (req, res) => {
+
+    try{
+      const userRequest = await Request.findAll({
+        include: [{ model: User }],
+        where: {user_id: 7},
+      });
+
+      console.log(userRequest)
+      const usrReq = userRequest.map(post => post.get({ plain: true }))
+      console.log(usrReq)
+      res.render('home', {usrReq,logged_in: req.session.logged_in});
+
+    }catch (err) {
+      res.status(500).json(err);
+      console.log
+    }
+  });
+
+
+
+
+
+
+   
+  
+
     // GET all users for admin page
 router.get('/admin', async (req, res) => {
   try {
@@ -31,7 +71,9 @@ router.get('/admin', async (req, res) => {
     map(post => post.get({ plain: true }))
     console.log(retUsers)
     res.render('admin',{retUsers,logged_in: req.session.logged_in});
+
   } catch (err) {
+    
     res.status(500).json(err);
     console.log
   }
