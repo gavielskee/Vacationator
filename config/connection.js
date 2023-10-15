1c2adfee79b2968e5e1c1a3cd12505ae64685998
gavielskee <gavielskee1@gmail.com>
2023-10-15 19:12:05 -0400

    JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,6 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 // Create a connection object
-const sequelize = new Sequelize(process.env.MYSQLURI);
+const sequelize = new Sequelize(process.env.JAWSDB_URL);
 
 module.exports = sequelize;
